61b023cd53c5a2c2afff0ceb84970c8a1499757e
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-16 05:00:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,38 +1,17 @@
-# class School
-#   attr_accessor :roster
-#
-#   def initialize(name)
-#     @name = name
-#     @roster = {}
-#   end
-#
-#   def add_student(name, grade)
-#     @roster.include?(grade) ? @roster[grade] << name : @roster[grade] = [name]
-#   end
-#
-#   def grade(grade)
-#     @roster[grade]
-#   end
-#
-#   def sort
-#     @roster.each{|grade, name| @roster[grade] = name.sort}
-#   end
-# end
-
 class School
-  attr_accessor :roster, :grade
+  attr_accessor :roster
 
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-  def add_student(name)
-    @roster.include?(@grade) ? @roster[@grade] << name : @roster[@grade] = [name]
+  def add_student(name, grade)
+    @roster.include?(grade) ? @roster[grade] << name : @roster[grade] = [name]
   end
 
-  def grade
-    @roster[@grade]
+  def grade(grade)
+    @roster[grade]
   end
 
   def sort
